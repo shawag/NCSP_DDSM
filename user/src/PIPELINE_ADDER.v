7d2ade597056c9ce1afb97846a472aaa4b9c674f
shawag <727627411@qq.com>
2024-11-08 09:39:00 +0800

    change pipeline adder element to lca

--- user/src/PIPELINE_ADDER.v
+++ user/src/PIPELINE_ADDER.v
@@ -1,60 +1,62 @@
 `include "Timescale.v"
 module PIPELINE_ADDER #(
-    parameter P_DATA_WIDTH = 6
+    parameter P_DATA_WIDTH = 8
 )
 (  
     input               i_clk   ,
     input               i_rst_n ,
-    input [P_DATA_WIDTH-1:0] i_a     ,
-    input [P_DATA_WIDTH-1:0] i_b     ,
-    input               i_cin   ,
-    output [P_DATA_WIDTH-1:0] o_sum  ,
-    output               o_cout   
+    input [7:0] i_a     ,
+    input [7:0] i_b     ,
+    input       i_cin   ,
+    output [7:0] o_sum  ,
+    output [7:0] o_cout_bit   
 );
 
-localparam P_HALF_DATA_WIDTH = P_DATA_WIDTH/2;
 
-wire cout_low;
-wire  [P_HALF_DATA_WIDTH-1:0] sum_high;
-wire  [P_HALF_DATA_WIDTH-1:0] sum_low;
+wire  [3:0] cout_low_bit;
+wire  [3:0] cout_high_bit; 
+wire  [3:0] sum_high;
+wire  [3:0] sum_low;
 
-reg [P_HALF_DATA_WIDTH-1:0]   r_sum_low;
-reg [P_HALF_DATA_WIDTH-1:0]   r_a_high;
-reg [P_HALF_DATA_WIDTH-1:0]   r_b_high;
+reg [3:0]   r_cout_bit_low;
+reg [3:0]   r_sum_low;
+reg [3:0]   r_a_high;
+reg [3:0]   r_b_high;
 
 always @(posedge i_clk or negedge i_rst_n) begin
     if (~i_rst_n) begin
-        r_sum_low <=  {P_HALF_DATA_WIDTH{1'b0}};
-        r_a_high   <= {P_HALF_DATA_WIDTH{1'b0}};
-        r_b_high   <= {P_HALF_DATA_WIDTH{1'b0}};
+        r_cout_bit_low <= {4{1'b0}};
+        r_sum_low <=  {4{1'b0}};
+        r_a_high   <= {4{1'b0}};
+        r_b_high   <= {4{1'b0}};
     end
     else begin
+        r_cout_bit_low <= cout_low_bit;
         r_sum_low <= sum_low;
-        r_a_high   <= i_a[(P_DATA_WIDTH)-1:P_HALF_DATA_WIDTH];
-        r_b_high   <= i_b[(P_DATA_WIDTH)-1:P_HALF_DATA_WIDTH];
+        r_a_high   <= i_a[7:4];
+        r_b_high   <= i_b[7:4];
     end
 end
 
 assign o_sum = {sum_high, r_sum_low};
+assign o_cout_bit = {cout_high_bit, r_cout_bit_low};
 
-ADDER_BASIC #(
-	.P_DATA_WIDTH 	( P_HALF_DATA_WIDTH ))
-u0_ADDER_BASIC(
-	.i_a    	( i_a[P_HALF_DATA_WIDTH-1:0]),
-	.i_b    	( i_b[P_HALF_DATA_WIDTH-1:0]),
+ADDER_LCA_4BIT 
+u0_ADDER_LCA_4BIT(
+	.i_a    	( i_a[3:0]),
+	.i_b    	( i_b[3:0]),
 	.i_cin  	( i_cin   ),
 	.o_sum  	( sum_low),
-	.o_cout 	( cout_low  )
+	.o_cout_bit ( cout_low_bit  )
 );
 
-ADDER_BASIC #(
-	.P_DATA_WIDTH 	( P_HALF_DATA_WIDTH ))
-u1_ADDER_BASIC(
+ADDER_LCA_4BIT 
+u1_ADDER_LCA_4BIT(
 	.i_a    	( r_a_high),
 	.i_b    	( r_b_high),
-	.i_cin  	( cout_low ),
+	.i_cin  	( cout_low_bit[3]),
 	.o_sum  	( sum_high),
-    .o_cout 	( o_cout   )
+    .o_cout_bit ( cout_high_bit  )
 );
 
 
